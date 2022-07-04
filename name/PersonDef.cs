49367e93e1d7371d83f5e9b3230a4d426e56893b
tarikul54 <tarikulkhan30@gmail.com>
2022-07-05 00:05:34 +0600

    add this

---
+++ name/PersonDef.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name
+{
+    class PersonDef
+    {
+        public string firstName;
+        public string middleName;
+        public string lastName;
+
+        public string GetfullName()
+        {
+            string FullName = firstName+" "+middleName+" "+lastName;
+
+            return FullName;
+        }
+    }
+}
---
+++ name/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonDef person = new PersonDef();
+            person.firstName = "Tarikul";
+            person.middleName = "Islam";
+            person.lastName = "Khan";
+            string FullName = person.GetfullName();
+            Console.WriteLine(FullName);
+        }
+    }
+}
